68609fc6ca653e51e4c376771933940badacb996
yousseflearn <youssefeasylearn@gmail.com>
2024-12-30 23:48:38 +0400

    Setup of Project and use shadcn/ui

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      Home Page <Button variant="link">Home</Button>
+    </div>
+  );
+}
