5095c81461892ec9c9b9c9d33e7fa9627df6d2de
yousseflearn <youssefeasylearn@gmail.com>
2025-01-01 22:05:12 +0400

    Creation Of Clerk authentication

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { UserButton } from '@clerk/nextjs';
+const NavBar = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+};
+
+export default NavBar;
