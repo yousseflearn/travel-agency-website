5cc0632b6ec174d7df118b1d8652a12d7ec2764a
yousseflearn <youssefeasylearn@gmail.com>
2025-01-03 20:47:31 +0400

    Creation of Navbar with Searching

---
+++ components/Container.tsx
@@ -0,0 +1,9 @@
+const Container = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="max-w-[1920px] w-full mx-auto xl:px-20 py-4">
+      {children}
+    </div>
+  );
+};
+
+export default Container;
--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,9 +1,49 @@
 'use client';
-import { UserButton } from '@clerk/nextjs';
+import { useAuth, UserButton } from '@clerk/nextjs';
+import Container from '../Container';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { Button } from '../ui/button';
+import SearchInput from '../SearchInput';
 const NavBar = () => {
+  const router = useRouter();
+  const { userId } = useAuth();
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
+    <div className="sticky top-0 border border-b-primary/10 bg-secondary">
+      <Container>
+        <div className="flex justify-between">
+          <div
+            className="flex items-center gap-1 cursor-pointer"
+            onClick={() => router.push('/')}
+          >
+            <Image src="/logo.jpg" alt="logo" width="30" height="30" />
+            <div className="font-bold text-xl ">Deer IT</div>
+          </div>
+          <SearchInput />
+          <div className="flex gap-3 items-center">
+            <div>theme</div>
+            <UserButton />
+            {!userId && (
+              <>
+                <Button
+                  onClick={() => router.push('/sign-in')}
+                  variant="outline"
+                  size="sm"
+                >
+                  Sign in
+                </Button>
+                <Button
+                  onClick={() => router.push('/sign-up')}
+                  variant="outline"
+                  size="sm"
+                >
+                  Sign up
+                </Button>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
